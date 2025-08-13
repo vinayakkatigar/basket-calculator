f7df10f7d266bf4880445e8600093f2700689e11
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2025-08-13 16:57:41 +0100

    TDD cases contract failed

--- src/test/java/com/shopping/cart/BasketCalculatorApplicationTests.java
+++ src/test/java/com/shopping/cart/BasketCalculatorApplicationTests.java
@@ -1,13 +1,39 @@
 package com.shopping.cart;
 
+import com.shopping.cart.basket.BasketCalculator;
+import com.shopping.cart.basket.impl.BasketCalculatorImpl;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.util.AssertionErrors;
 
 @SpringBootTest
 class BasketCalculatorApplicationTests {
 
+    private BasketCalculator objectUnderTest;
+
+    @BeforeTestClass
+    public void setUp(){
+        objectUnderTest = new BasketCalculatorImpl();
+    }
+
 	@Test
-	void contextLoads() {
+	void testEmptyItems() {
+        AssertionErrors.fail("To-Do");
+	}
+
+	@Test
+	void testOnlyAppleBasketItems() {
+        AssertionErrors.fail("To-Do");
+	}
+	@Test
+	void testOnlyOrangeBasketItems() {
+        AssertionErrors.fail("To-Do");
+	}
+	@Test
+	void testAppleAndOrangeBasketItems() {
+        AssertionErrors.fail("To-Do");
 	}
 
 }
