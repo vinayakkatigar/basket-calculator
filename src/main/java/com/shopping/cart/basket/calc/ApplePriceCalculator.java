f355d56e6d02d1ff69a8818eb1f39cf57a9cdbae
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2025-08-13 18:48:42 +0100

    refactor

--- src/main/java/com/shopping/cart/basket/calc/ApplePriceCalculator.java
+++ src/main/java/com/shopping/cart/basket/calc/ApplePriceCalculator.java
@@ -7,14 +7,14 @@ import static java.math.BigDecimal.valueOf;
 
 public class ApplePriceCalculator {
 
-    public static double discountRate = 0.5;
+    public static double twoForOneDiscountRate = 0.5;
     public static double price = 60;
 
     public BigDecimal calcPrice(List<String> appleBasketItems) {
         int itemsSize = appleBasketItems.size();
         if (itemsSize > 0 ){
             if (itemsSize % 2 == 0 ){
-                return valueOf((itemsSize) * price * discountRate);
+                return valueOf((itemsSize) * price * twoForOneDiscountRate);
             }else if (itemsSize > 2){
                 int remainderOfTwo = itemsSize % 2;
                 return valueOf((itemsSize / 2) * price)
