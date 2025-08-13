c6f2fc8f66f173a4de45e6d630476d63c7ab7453
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2025-08-13 16:45:36 +0100

    Initial commit
    Initial commit

---
+++ src/main/java/com/shopping/cart/basket_calculator/BasketCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.cart.basket_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping/cart/basket_calculator/BasketCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.cart.basket_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
